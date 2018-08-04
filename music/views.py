f3a5c387e6b2476f3c65e5fa59bcc647e75934f5
pardeep11 <pawasthy11@gmail.com>
2018-08-04 19:09:36 +0530

    views.py add

--- music/views.py
+++ music/views.py
@@ -1,7 +1,13 @@
 # -*- coding: utf-8 -*-
 from django.http  import HttpResponse
-
 from django.shortcuts import render
+from .models import Album
+
 
 def index(request):
-    return HttpResponse("<h1>music website</h1>")
+    all_albums= Album.objects.all()
+    context={'all_albums':all_albums}
+    return render(request,'music/index.html',context)
+
+def detail(request,album_id):
+    return HttpResponse("<h1>hello " + str(album_id) + "</h1> ")
