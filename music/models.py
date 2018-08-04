2973e58cde5a4c96a65cded3c87c70fee1ea2195
pardeep11 <pawasthy11@gmail.com>
2018-08-04 19:17:28 +0530

    models update

--- music/models.py
+++ music/models.py
@@ -8,6 +8,11 @@ class Album(models.Model):
     album_title=models.CharField(max_length=100)
     genre=models.CharField(max_length=100)
     album_logo=models.CharField(max_length=1000)
+ 
+    def __str__(self):
+        return self.album_title +'-'+ self.artist
+
+
 
 class song(models.Model):
     album=models.ForeignKey(Album,on_delete=models.CASCADE)
